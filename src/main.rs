9188a602246e33f8e87e6dd1ef904c17933bd797
Shubhamai <shubham.aiengineer@gmail.com>
2022-11-11 21:57:59 +0530

    experimenting with different camera crate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod model;
 mod webcam;
 
+use image::{imageops::resize, ImageBuffer, Rgb};
 use model::use_onnxruntime;
-use webcam::use_nokhwa;
+use webcam::{initialize_nokhwa_camera, initialize_camera_capture, frame2ndarray};
 
 use onnxruntime::tensor::OrtOwnedTensor;
 
+use std::time::{Instant};
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Camera
-    let mut camera = use_nokhwa::initialize_camera(0, 120, 120, 30)?;
+    // let mut camera = initialize_nokhwa_camera(0, 120, 120, 30)?;
+    let mut camera = initialize_camera_capture(0, 720, 1280, 30.0)?;
 
     // Model
     let env = use_onnxruntime::get_environment(&"Landmark Detection")?;
@@ -16,18 +21,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         use_onnxruntime::initialize_model(&env, "./assets/mb1_120x120.onnx".to_string(), 1)?;
 
     loop {
+        let start_time = Instant::now();
+
         // New frame
-        let frame = camera.frame()?;
+        // let frame = camera.frame()?;
+        let frame = camera.next().unwrap();
+
+        // Resize
+        let frame_buffer:ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_vec(120, 120, frame.to_vec()).unwrap();
+        let resized_frame = resize(&frame_buffer, 120, 120, image::imageops::FilterType::Nearest); 
 
         // Processing the inputs
-        let input = use_nokhwa::frame2ndarray(
-            frame.to_vec(),
+        let input = frame2ndarray(
+            resized_frame.to_vec(),
             model.inputs[0].dimensions().map(|d| d.unwrap()).collect(),
         )?;
 
         // Generating the outputs
         let outputs: Vec<OrtOwnedTensor<f32, _>> = model.run(vec![input])?;
 
-        println!("{:?}", outputs)
+        // println!("{:?}", outputs)
+
+        let elapsed_time = start_time.elapsed();
+        println!("delay : {}", elapsed_time.as_millis());
     }
 }
--- src/webcam.rs
+++ src/webcam.rs
@@ -1,49 +1,64 @@
-use {nokhwa, onnxruntime};
+use camera_capture::{self, Builder, ImageIterator};
+use nokhwa::{Camera, CameraFormat, CameraInfo, FrameFormat, NokhwaError};
+use onnxruntime::ndarray::{Array, ArrayBase, Dim, IxDynImpl, OwnedRepr};
 
-pub mod use_nokhwa {
+#[cfg(unix)]
+use nokhwa::CameraIndex;
 
-    use super::onnxruntime::ndarray::{Array, ArrayBase, Dim, IxDynImpl, OwnedRepr};
+// To get all the camera info
+pub fn _get_cameras_info() -> Result<Vec<CameraInfo>, NokhwaError> {
+    nokhwa::query_devices(nokhwa::CaptureAPIBackend::Auto)
+}
 
-    use super::nokhwa::{Camera, CameraFormat, CameraInfo, FrameFormat, NokhwaError};
+// Initializing a camera
+pub fn initialize_nokhwa_camera(
+    index: u32,
+    height: u32,
+    width: u32,
+    fps: u32,
+) -> Result<Camera, Box<dyn std::error::Error>> {
     #[cfg(unix)]
-    use super::nokhwa::CameraIndex;
+    let camera_index = CameraIndex::Index(index);
 
+    #[cfg(windows)]
+    let camera_index: usize = index.try_into().unwrap();
 
-    // To get all the camera info
-    pub fn _get_cameras_info() -> Result<Vec<CameraInfo>, NokhwaError> {
-        nokhwa::query_devices(nokhwa::CaptureAPIBackend::Auto)
-    }
+    let mut camera = Camera::new(
+        &camera_index,
+        Some(CameraFormat::new_from(
+            width,
+            height,
+            FrameFormat::MJPEG,
+            fps,
+        )),
+    )?;
+    camera.open_stream()?;
 
-    // Initializing a camera
-    pub fn initialize_camera(
-        index: u32,
-        height: u32,
-        width:u32,
-        fps: u32,
-    ) -> Result<Camera, Box<dyn std::error::Error>> {
+    Ok(camera)
+}
 
-        #[cfg(unix)]
-        let camera_index = CameraIndex::Index(index);
+pub fn initialize_camera_capture(
+    index: u32,
+    height: u32,
+    width: u32,
+    fps: f64,
+) -> Result<ImageIterator, Box<(dyn std::error::Error + 'static)>> {
+    let cam_builder = camera_capture::create(index).unwrap();
 
-        #[cfg(windows)]
-        let camera_index:usize = index.try_into().unwrap();
+    Ok(cam_builder
+        .fps(fps)
+        .unwrap()
+        .resolution(width, height)
+        .unwrap()
+        .start()?)
+}
 
-        let mut camera = Camera::new(
-            &camera_index,
-            Some(CameraFormat::new_from(width, height, FrameFormat::MJPEG, fps)),
-        )?;
-        camera.open_stream()?;
+// Processing the input
+pub fn frame2ndarray(
+    frame: Vec<u8>,
+    input_shape: Vec<usize>,
+) -> Result<ArrayBase<OwnedRepr<f32>, Dim<IxDynImpl>>, Box<dyn std::error::Error>> {
+    let frame_array: Vec<f32> = frame.iter().map(|&e| e as f32).collect();
 
-        Ok(camera)
-    }
-
-    // Processing the input
-    pub fn frame2ndarray(
-        frame: Vec<u8>,
-        input_shape: Vec<usize>,
-    ) -> Result<ArrayBase<OwnedRepr<f32>, Dim<IxDynImpl>>, Box<dyn std::error::Error>> {
-        let frame_array: Vec<f32> = frame.iter().map(|&e| e as f32).collect();
-
-        Ok(Array::from_vec(frame_array).into_shape(input_shape)?)
-    }
+    Ok(Array::from_vec(frame_array).into_shape(input_shape)?)
 }
